a26f9d880bfc657638435b4094fe9c78a7ccccb5
fan535607581 <56910554+fan535607581@users.noreply.github.com>
2020-01-17 18:19:34 +0800

    Update SocketClient.java

--- SocketClient.java
+++ SocketClient.java
@@ -71,7 +71,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
         }else{  GetMessage("连接未创建！"); }
     }
     @SimpleFunction(description = "start")
-    public void sendMessage(String s , int ms)
+    public void sendMessage(String s)
     {  
 	 int k = s.length()/3;
         if(socket != null){
@@ -152,7 +152,7 @@ public class SocketClient extends AndroidNonvisibleComponent {
                 case SENDMESSAGE:
                     try {
 			byte[] bb = new byte[255]; 
-			for(int j = 0; j<js+1 ;j++){bb[j+1] = (byte)((i[j] >> 0));}
+			for(int j = 0; j<js+1 ;j++){bb[j+1] = (byte)i[j];}
 			ou.write(bb , 1 , js);    
                         msg = myHandler.obtainMessage();
                         msg.obj = "发送完毕";
